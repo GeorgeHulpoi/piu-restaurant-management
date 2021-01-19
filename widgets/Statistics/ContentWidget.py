8e0740ca05d294fb33e2c3a9c6e7a8566da17179
Acar Valeriu-Mihai <valeriu-mihai.acar@student.tuiasi.ro>
2021-01-19 20:44:11 +0200

    update: wrapped file open in try/catch
    Signed-off-by: Acar Valeriu-Mihai <valeriu-mihai.acar@student.tuiasi.ro>

--- widgets/Statistics/ContentWidget.py
+++ widgets/Statistics/ContentWidget.py
@@ -41,53 +41,56 @@ class ContentWidget(QWidget):
             '12:00 AM', '01:00 PM', '02:00 PM', '03:00 PM'], [3, 10, 20, 1], self))
 
     def readDataPieChart(self, dirname, out_dictionary):
-        # auxiliary dictionary for local use
-        dictionary = {}
+        try:
+            # auxiliary dictionary for local use
+            dictionary = {}
 
-        # access from root folder, subdirectories and files
-        for root, subdirs, files in os.walk(dirname):
+            # access from root folder, subdirectories and files
+            for root, subdirs, files in os.walk(dirname):
 
-            # for every file in files
-            for filename in files:
+                # for every file in files
+                for filename in files:
 
-                # ignore if the file indicates the dummy.txt
-                if not filename.__eq__("dummy.txt"):
+                    # ignore if the file indicates the dummy.txt
+                    if not filename.__eq__("dummy.txt"):
 
-                    # get the relative path from root to the actual file
-                    file_path = os.path.join(root, filename)
+                        # get the relative path from root to the actual file
+                        file_path = os.path.join(root, filename)
 
-                    # open the file
-                    with open(file_path) as json_file:
+                        # open the file
+                        with open(file_path) as json_file:
 
-                        # read and load data
-                        data = json.load(json_file)
+                            # read and load data
+                            data = json.load(json_file)
 
-                        # iterate and create a dictionary where the key is name of the product
-                        # and the value is the quantity of the product. also, if product already
-                        # exists, increment they value for that key
-                        for item in data["order-list"]:
-                            if item["name"] in dictionary:
-                                dictionary[item["name"]] += item["qty"]
-                            else:
-                                dictionary[item["name"]] = item["qty"]
+                            # iterate and create a dictionary where the key is name of the product
+                            # and the value is the quantity of the product. also, if product already
+                            # exists, increment they value for that key
+                            for item in data["order-list"]:
+                                if item["name"] in dictionary:
+                                    dictionary[item["name"]] += item["qty"]
+                                else:
+                                    dictionary[item["name"]] = item["qty"]
 
-        # order dictionary in descending order
-        sorted_dictionary = collections.OrderedDict(
-            sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True))
+            # order dictionary in descending order
+            sorted_dictionary = collections.OrderedDict(
+                sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True))
 
-        # try get first top 4 most sold items
-        if len(sorted_dictionary) >= 4:
-            count = 0
-            for x in sorted_dictionary:
-                out_dictionary[x] = sorted_dictionary[x]
+            # try get first top 4 most sold items
+            if len(sorted_dictionary) >= 4:
+                count = 0
+                for x in sorted_dictionary:
+                    out_dictionary[x] = sorted_dictionary[x]
 
-                if count < 3:
-                    count += 1
-                else:
-                    break
-        # if there are not 4 items but len still is greater than 0
-        elif len(sorted_dictionary) > 0:
-            for x in sorted_dictionary:
-                out_dictionary[x] = sorted_dictionary[x]
-        else:
-            out_dictionary["Product"] = 1
+                    if count < 3:
+                        count += 1
+                    else:
+                        break
+            # if there are not 4 items but len still is greater than 0
+            elif len(sorted_dictionary) > 0:
+                for x in sorted_dictionary:
+                    out_dictionary[x] = sorted_dictionary[x]
+            else:
+                out_dictionary["Product"] = 1
+        except Exception as e:
+            print(e)
