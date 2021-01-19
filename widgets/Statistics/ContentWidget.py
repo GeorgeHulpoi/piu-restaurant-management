96c641d34205fd047485786c251cf0b769d7fc6c
Acar Valeriu-Mihai <valeriu-mihai.acar@student.tuiasi.ro>
2021-01-19 20:35:30 +0200

    update: real time support for piechart
    Signed-off-by: Acar Valeriu-Mihai <valeriu-mihai.acar@student.tuiasi.ro>

--- widgets/Statistics/ContentWidget.py
+++ widgets/Statistics/ContentWidget.py
@@ -1,5 +1,9 @@
+import os
+import json
+import collections
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QHBoxLayout
 
 from widgets.Statistics.BarChartWidget import BarChartWidget
 from widgets.Statistics.PieChartWidget import PieChartWidget
@@ -13,9 +17,77 @@ class ContentWidget(QWidget):
         self.setUi()
 
     def setUi(self):
+        piechart_data = {}          # initialise empty dictionary for piechart data
+        piechart_labels = []        # initialise empty list for piechart labels
+        piechart_quantities = []    # initialise empty list for piechart percentages
+
+        # read the data recursevly from existing files
+        self.readDataPieChart(
+            f"{os.path.dirname(__file__)}/../TableOrderView/orders",
+            piechart_data)
+
+        # extract the data from the dictionary and append it to it's specific list
+        for x in piechart_data:
+            piechart_labels.append(x)
+            piechart_quantities.append(piechart_data[x])
+
         self.setLayout(QHBoxLayout())
+        self.setAttribute(Qt.WA_StyledBackground, True)
         self.layout().setContentsMargins(32, 0, 32, 32)
         self.layout().setSpacing(0)
-        self.setAttribute(Qt.WA_StyledBackground, True)
-        self.layout().addWidget(PieChartWidget('Most Bought Products', ['Item 1', 'Item 2', 'Item 3', 'Item 4'], [10, 20, 30, 40], self))
-        self.layout().addWidget(BarChartWidget('Time Clients', ['12:00 AM', '01:00 PM', '02:00 PM', '03:00 PM'], [3, 10, 20, 1], self))
+        self.layout().addWidget(PieChartWidget('Most Bought Products',
+                                               piechart_labels, piechart_quantities, self))
+        self.layout().addWidget(BarChartWidget('Time Clients', [
+            '12:00 AM', '01:00 PM', '02:00 PM', '03:00 PM'], [3, 10, 20, 1], self))
+
+    def readDataPieChart(self, dirname, out_dictionary):
+        # auxiliary dictionary for local use
+        dictionary = {}
+
+        # access from root folder, subdirectories and files
+        for root, subdirs, files in os.walk(dirname):
+
+            # for every file in files
+            for filename in files:
+
+                # ignore if the file indicates the dummy.txt
+                if not filename.__eq__("dummy.txt"):
+
+                    # get the relative path from root to the actual file
+                    file_path = os.path.join(root, filename)
+
+                    # open the file
+                    with open(file_path) as json_file:
+
+                        # read and load data
+                        data = json.load(json_file)
+
+                        # iterate and create a dictionary where the key is name of the product
+                        # and the value is the quantity of the product. also, if product already
+                        # exists, increment they value for that key
+                        for item in data["order-list"]:
+                            if item["name"] in dictionary:
+                                dictionary[item["name"]] += item["qty"]
+                            else:
+                                dictionary[item["name"]] = item["qty"]
+
+        # order dictionary in descending order
+        sorted_dictionary = collections.OrderedDict(
+            sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True))
+
+        # try get first top 4 most sold items
+        if len(sorted_dictionary) >= 4:
+            count = 0
+            for x in sorted_dictionary:
+                out_dictionary[x] = sorted_dictionary[x]
+
+                if count < 3:
+                    count += 1
+                else:
+                    break
+        # if there are not 4 items but len still is greater than 0
+        elif len(sorted_dictionary) > 0:
+            for x in sorted_dictionary:
+                out_dictionary[x] = sorted_dictionary[x]
+        else:
+            out_dictionary["Product"] = 1
